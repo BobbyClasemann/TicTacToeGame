58de9c65077266c732336c4077ec485e61bb4a40
Dawood Khan <khandawood15@gmail.com>
2018-08-18 15:31:54 -0500

    Worked on checkWinningCondition NOT COMPLETE

--- JS/interact.js
+++ JS/interact.js
@@ -22,3 +22,36 @@ function processTurn(ID){
     }
 
 }
+
+function checkWinningCondition(){
+    var tableObj = document.getElementById("tictactoe");
+
+    var size = document.getElementById("tictactoe").rows.length;
+    // making a 3x3 array in JS
+    var grid = new Array(size);
+    for (var i = 0; i < size; i++)
+        grid[i] = new Array(size);
+
+
+    for (var i = 0; i < size; i++)
+        for (var j = 0; j < size; j++){
+            console.log('i: ' + i + " j: " + j);
+            console.log("contents: " + tableObj.rows[i].cells.item(j).innerHTML)
+            grid[i][j] = tableObj.rows[i].cells.item(j).innerHTML
+        }
+
+    console.log(grid);
+
+    var win = true; // assume there is a win
+    var char; // to test if all along row, col, or diagonal are same
+    // now checking winning conditions, every row and column and 2 diagonals are possible winning conditions
+    for (var i = 0; i < grid.length && !win; i++) {
+        char = grid[i][0]; // check if all characters along the row are the same
+        for (var j = 0;j < grid[i].length && !win; j++){
+            if (!(char === grid[i][j]))
+        }
+    }
+
+
+
+}
