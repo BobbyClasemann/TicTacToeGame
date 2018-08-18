07023b9efbeec90da97b5552ab2ab8fd7b3f9a62
BobbyClasemann <clasemannbobby@gmail.com>
2018-08-18 14:12:20 -0500

    Added function to change content of tictactoe nodes but doesn't work.

--- JS/interact.js
+++ JS/interact.js
@@ -1,7 +1,24 @@
-function dawood() {
-    document.getElementById("00").innerHTML = "Hello World!";
-}
+function processTurn(ID){
+    var content = document.getElementById(ID).value;
+    console.log(content);
 
-function wood() {
-    document.getElementById("01").innerHTML = "Wood";
+    //cannot change the existing element
+    if (content == "O" || content == "X") {
+        return;
+    }
+
+    var playChar = document.getElementById("playChar").value;   //get current play character
+    console.log(playChar);
+    document.getElementById(ID).value = playChar;               //change node to current play character
+
+    //reset hidden filed to next player's character
+    if (playChar == "X") {
+        document.getElementById("playChar").value = "O";        //change current play char to O
+        console.log("O");
+    }
+    else {
+        document.getElementById("playChar").value = "X";        //change current play char to X
+        console.log("X");
+    }
+
 }
