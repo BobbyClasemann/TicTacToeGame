e2854d0ca4026ae4ae42523c492ecce11f4a0aff
khanx263 <khanx263@umn.edu>
2018-12-30 16:37:44 -0600

    Added alot of AI helper functions

--- JS/interact.js
+++ JS/interact.js
@@ -43,7 +43,7 @@ function displayWinMsg(arr){
     }
 }
 
-function getGrid() {
+function getBoard() {
     var tableObj = document.getElementById("tictactoe");
 
     var size = document.getElementById("tictactoe").rows.length;
@@ -59,7 +59,7 @@ function getGrid() {
         }
     }
 
-    return [grid, size];
+    return grid;
 
 }
 
@@ -70,10 +70,9 @@ function displayDrawMsg(){
     document.getElementById("playingChar").value = "Draw";
 }
 
-function checkDrawCondition(){
-    var arr = getGrid();
-    var grid = arr[0];
-    var size = arr[1];
+function checkDrawCondition(board = getBoard()){
+    var grid = board;
+    var size = 3;
 
     var slotsFilled = 0;
 
@@ -90,10 +89,9 @@ function checkDrawCondition(){
 }
 
 
-function checkWinningCondition(){
-    var arr = getGrid();
-    var grid = arr[0];
-    var size = arr[1];
+function checkWinningCondition(board = getBoard()){
+    var grid = board;
+    var size =3;
     var win = false; // assume there is not a win
     var char;
     // now checking winning conditions, every row and column and 2 diagonals are possible winning conditions
@@ -184,3 +182,179 @@ function resetGame(){
     document.getElementById("playingChar").value = "X";    //reset to player 1
     document.getElementById("playerTurn").innerHTML = "Player 1's Turn";    //reset to player 1's turn
 }
+
+// returns a character representing the endgame from the perspective of ai Player
+// board is a 3x3 grid of X's and O's
+// aiPlayer is either an X or an O
+function getEndGame (board, aiPlayer) {
+    var arr = checkWinningCondition(board);
+
+    console.log(arr);
+    console.log(checkDrawCondition(board));
+    if (checkDrawCondition(board) && !arr[0]) {
+        return 'D';                  //return draw if draw condition is true and no winner\
+    }
+    else if(arr[0]){
+        if (arr[1] === aiPlayer)    //check if player that won is the ai
+            return 'W';             //return win if it is
+        else
+            return 'L';             //return loss if is not
+    }
+    else {
+        return "";                 //error check case
+    }
+}
+
+// receives a 3x3 boolean array and checks if there are any T values remaining in the array
+function checkPossibleMovesLeft(movesGrid){
+
+    for(var i = 0; i < 3; i++){
+        for(var j = 0; j < 3; j++){
+            if(movesGrid[i][j])
+                return true;// possible move exists
+        }
+    }
+
+    return false;
+}
+
+// get next available position on the board
+function getNextAvailablePosition(movesGrid){
+
+    for(var i = 0; i < 3; i++){
+        for(var j = 0; j < 3; j++){
+            if(movesGrid[i][j])
+                return [i, j];// next possible move
+        }
+    }
+
+    return false; // error
+}
+
+
+// determines which character/player should go next,
+// Note: does not check for endgames
+function whoseTurn(board){
+    xCount = 0;
+    oCount = 0;
+
+    for(var i = 0; i < 3; i++){
+        for(var j = 0; j < 3; j++){
+            if(board[i][j] == 'X'){
+                xCount++;
+            } else if(board[i][j] == 'O'){
+                oCount++;
+            }
+        }
+    }
+
+    // the count of X's and O's will always be the same or the count of X's will exceed the count of O's by 1
+    if(xCount == oCount){
+        return 'X'
+    } else {
+        return 'O';
+    }
+}
+
+function getMinElement (arr){
+    //if array has no values, return 0; error case
+    if (arr.length == 0)
+        return false;
+
+    var min = arr[0];   //assume first value in array is the min
+
+    //compare min to every value in array to find actual minimum value
+    for (var i = 1; i < arr.length; i++){
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    return min;
+}
+
+function makeMoveGrid (board){
+    var grid = boardCopy(board);  //make a copy of the board
+
+    //if the board slot contains an X or an O
+    for (var i = 0; i < grid.length; i++){
+        for (var j = 0; j < grid[i].length; j++){
+            if (grid[i][j] === 'X' || grid[i][j] === 'O'){
+                grid[i][j] = false; //not an available move slot
+            }
+            else
+                grid[i][j] = true;  //available move slot
+        }
+    }
+
+    return grid;
+}
+
+// returns a copy of the board
+function boardCopy(arr){
+    var bCopy = [];
+    for (var i = 0; i < arr.length; i++){
+        bCopy.push([]);
+        for(var j = 0; j < arr[i].length; j++) {
+            bCopy[i].push(arr[i][j]);
+        }
+    }
+    return bCopy;
+}
+
+// selects the maximum score on the board consisting of X's, O's, and numbers
+function getMaxScoreBoardPos(scoreBoard){
+    var maxPos = false;// sentinel value - board only consists of X's and O's
+    var maxVal = -9999;
+
+    for (var i = 0; i < scoreBoard.length; i++){
+        for (var j = 0; j < scoreBoard[i].length; j++){
+            if (scoreBoard[i][j] != 'X' || scoreBoard[i][j] != 'O'){
+                if (maxVal < scoreBoard[i][j]){
+                    maxVal = scoreBoard[i][j];
+                    maxPos = [i, j];
+                }
+            }
+        }
+    }
+
+    return maxPos;
+}
+
+var items = [
+    ['X', 'O', 'O'],
+    ['X', 'O', 'X'],
+    ['O', 'X', 'O']
+];
+
+alert(getMaxScoreBoardPos(items));
+
+/*
+var arr =  [];
+alert(getMinElement(arr));
+*/
+
+//var aiPlayer = 'X';
+// var items = [
+//     ['', 'X', ''],
+//     ['O', '', ''],
+//     ['', '', '']
+// ];
+// var bobby = makeMoveGrid(items);
+// console.log(items);
+// console.log(bobby);
+// alert(makeMoveGrid(items));
+// alert(items);
+//alert(getEndGame(items, aiPlayer));
+
+
+
+/*
+var items = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false]
+];
+
+alert(checkPossibleMovesLeft(items));
+*/
+
